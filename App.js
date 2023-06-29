c4dd6cc7e00ed57dc35cf390287d1201c4c87f7c
OleksandrLysenko777 <direst2010@gmail.com>
2023-06-29 13:21:43 +0200

    last hw

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "./Screens/Authorization/LoginScreen/LoginScreen";
+import RegistrationScreen from "./Screens/Authorization/RegistrationScreen/RegistrationScreen";
+import Home from "./Screens/Home/Home";
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+const MainStack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+  if (fontsLoaded) {
+    return (
+      <View style={styles.container}>
+        <NavigationContainer>
+          <MainStack.Navigator
+            initialRouteName="Login"
+            screenOptions={{ headerShown: false }}
+          >
+            <MainStack.Screen
+              name="Registration"
+              component={RegistrationScreen}
+            />
+            <MainStack.Screen name="Login" component={LoginScreen} />            
+            <MainStack.Screen name="Home" component={Home} />           
+          </MainStack.Navigator>
+        </NavigationContainer>
+      </View>
+
+      // <RegistrationScreen style={styles.container}>
+      //   <Text>Open up App.js to start working on your app!</Text>
+      //   <StatusBar style="auto" />
+      // </RegistrationScreen>
+
+      // <LoginScreen style={styles.container}>
+      //   <Text>Open up App.js to start working on your app!</Text>
+      //   <StatusBar style="auto" />
+      // </LoginScreen>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
